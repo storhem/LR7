38e0e4ad8dfd2d99038db52b6a18577f01922041
storhem <storhetmax@mail.ru>
2025-12-26 21:57:06 +0300

    delete extra code

--- sqltransactions.py
+++ sqltransactions.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine, text, select, update, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker, Session
-from sqlalchemy.exc import SQLAlchemyError
-from contextlib import contextmanager
 from typing import List
 from pydantic import BaseModel
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, future=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=Session)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
 app = FastAPI(title="Транзакции с FastAPI и SQLAlchemy")
 
@@ -68,13 +66,12 @@ class TransactionRequest(BaseModel):
     to_user_id: int
     amount: float
 
-@contextmanager
 def get_db():
     db = SessionLocal()
     try:
         yield db
         db.commit()
-    except SQLAlchemyError:
+    except Exception:
         db.rollback()
         raise
     finally:
